ef77a41a3271ae700779373eec14df3b2ae388a1
Florent Becouze <florent.becouze@etu.uca.fr>
2018-12-14 13:09:47 +0100

    Ajout des RadioButton sur l'Accueil, essais chrono

--- code/src/sample/gui/controller/AccueilController.java
+++ code/src/sample/gui/controller/AccueilController.java
@@ -1,11 +1,17 @@
 package sample.gui.controller;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 import sample.launcher.Main;
@@ -21,8 +27,28 @@ public class AccueilController extends BaseController {
     private static final String BATAILLE_FXML = "../view/vueBataille.fxml";
     private static final String INFOS_FXML = "../view/vueInfos.fxml";
 
+    @FXML
+    private RadioButton boutonFacile;
+    @FXML
+    private RadioButton boutonNormal;
+    @FXML
+    private RadioButton boutonDifficile;
+
+
+
     public AccueilController(Stage stage) {
         super(stage);
+    }
+
+    @FXML
+    private void initialize()
+    {
+        // Permet de créer un groupe de RadioButton et donc il n'y a qu'un RadioButton qui peut être coché
+        ToggleGroup group = new ToggleGroup();
+        boutonFacile.setToggleGroup(group);
+        boutonNormal.setToggleGroup(group);
+        boutonDifficile.setToggleGroup(group);
+
     }
 
     @FXML
@@ -37,4 +63,6 @@ public class AccueilController extends BaseController {
     private void infos(ActionEvent actionEvent) throws IOException {
         openStage(INFOS_FXML, 800, 500);
     }
+
+
 }
--- code/src/sample/gui/controller/BatailleController.java
+++ code/src/sample/gui/controller/BatailleController.java
@@ -1,16 +1,19 @@
 package sample.gui.controller;
 
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import sample.gui.view.Grille;
 import sample.launcher.Main;
-import sample.model.Partie;
 
 import java.net.URL;
+import java.time.chrono.ChronoPeriod;
+import java.time.chrono.ChronoZonedDateTime;
+import java.util.concurrent.TimeUnit;
 
 
 public class BatailleController extends BaseController {
@@ -20,6 +23,9 @@ public class BatailleController extends BaseController {
     Grille playerGrid;
 
     private static final String PAUSE_FXML = "../view/vuePause.fxml";
+
+    @FXML
+    private Label chrono;
 
     // Constructor call before View init
     public BatailleController(Stage stage) {
@@ -34,7 +40,53 @@ public class BatailleController extends BaseController {
     // Call when View elements are initialized
     @FXML
     private void initialize() {
-        //iaGrid.configureButtons(getCurrGame().getIaGrid());
+	//iaGrid.configureButtons(getCurrGame().getIaGrid());
+        
+	creerChrono();
+    }
+
+    private void creerChrono() {
+        int minutes = 0;
+        int secondes = 0;
+        int heures = 0;
+        int j = 0;
+
+        while (j<10)
+        {
+            for(int i = 0; i<60; i++)
+            {
+                secondes++;
+                if(secondes == 60)
+                {
+                    minutes++;
+                    secondes = 0;
+                }
+                if(minutes == 60)
+                {
+                    heures++;
+                    minutes = 0;
+                }
+
+                if(secondes < 10)
+                {
+                    chrono.setText(heures + ":" + minutes + ":0" + secondes);
+                }
+                else{
+                    chrono.setText(heures + ":" + minutes + ":" + secondes);
+                }
+
+                if(minutes < 10)
+                {
+                    chrono.setText(heures + ":0" + minutes + ":" + secondes);
+                }
+                else{
+                    chrono.setText(heures + ":" + minutes + ":" + secondes);
+                }
+
+            }
+
+            j++;
+        }
     }
 
     @FXML
